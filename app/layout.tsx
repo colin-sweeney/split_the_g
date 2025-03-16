301e7d8572ed46982fb2eed7f86e74c4a531e138
colin-sweeney <colinsweeney1124@gmail.com>
2025-03-16 19:55:26 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ export const metadata: Metadata = {
   title: 'Split the G',
   description: 'Join the movement to redefine greatness through the viral Split The G Challenge',
   generator: 'v0.dev',
+  icons: {
+    icon: '/favicon.svg', // or '/favicon.png' or '/favicon.svg' depending on your file
+    // You can also specify different sizes for Apple devices
+    apple: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({
