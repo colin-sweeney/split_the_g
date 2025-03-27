7041f45aca93a6bbdd592c482706f43a4ddc8860
colin-sweeney <colinsweeney1124@gmail.com>
2025-03-26 22:37:37 -0400

    Create donate-button.tsx
    creating donate button

---
+++ components/donate-button.tsx
@@ -0,0 +1,33 @@
+donate-button.tsx
+
+'use client'
+
+import { Button, ButtonProps } from "@/components/ui/button"
+import { FC } from "react"
+
+interface DonateButtonProps extends Omit<ButtonProps, 'onClick'> {
+  donationUrl?: string
+}
+
+const DonateButton: FC<DonateButtonProps> = ({ 
+  donationUrl = 'https://www.every.org/foreignaidbridgefund/f/split-the-g-for-global',
+  className = "bg-[#E94B3C] text-white hover:bg-[#C7392D] font-bold transform hover:scale-105 transition-transform shadow-lg",
+  children = "Donate Now",
+  ...props
+}) => {
+  const handleClick = () => {
+    window.open(donationUrl, '_blank')
+  }
+
+  return (
+    <Button 
+      className={className}
+      onClick={handleClick}
+      {...props}
+    >
+      {children}
+    </Button>
+  )
+}
+
+export default DonateButton
